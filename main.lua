ca9b7a16c9c6dc04e8b502a73beca470bee7e72a
HongBloodGod <44256225+HongBloodGod@users.noreply.github.com>
2018-10-24 19:11:27 +0800

    更新2
    新增較大的藍球

--- main.lua
+++ main.lua
@@ -34,7 +34,8 @@ local params = {
 	{ radius=18, xDir=1, yDir=1, xSpeed=2.5, ySpeed=6.0, r=1, g=0, b=0.1 },
 	{ radius=15, xDir=1, yDir=1, xSpeed=3.5, ySpeed=4.0, r=0.95, g=0.1, b=0.3 },
 	{ radius=22, xDir=1, yDir=-1, xSpeed=5.5, ySpeed=5.0, r=0.9, g=0.2, b=0.5 },
-	{ radius=50, xDir=1, yDir=-1, xSpeed=8, ySpeed=8, r=128, g=0, b=0 }
+	{ radius=50, xDir=1, yDir=-1, xSpeed=8, ySpeed=8, r=128, g=0, b=0 },
+	{ radius=50, xDir=1, yDir=-1, xSpeed=9, ySpeed=9, r=0, g=0, b=128 }
 }
 
 local ballCollection = {}
